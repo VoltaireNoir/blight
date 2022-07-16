03d543c3ad923022f0b9016ef90fbef673634a45
VoltaireNoir <mzahmed95@gmail.com>
2022-07-17 00:32:48 +0530

    Use enum Direction to determine pos-neg

--- src/main.rs
+++ src/main.rs
@@ -55,17 +55,23 @@ impl Device {
 
 }
 
+enum Direction {
+    Inc,
+    Dec
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let (inc, dec) = (Direction::Inc, Direction::Dec);
 
     if args.len() > 1 {
         match &args[1..] {
-            [dir,step_size] if dir == "inc" => change_bl(step_size, dir),
-            [dir,step_size] if dir == "dec" => change_bl(step_size, dir),
+            [dir,step_size] if dir == "inc" => change_bl(step_size, inc),
+            [dir,step_size] if dir == "dec" => change_bl(step_size, dec),
             [case,value] if case == "set" => set_bl(value),
             [case,value] if case == "set" => set_bl(value),
-            [dir] if dir == "inc" => change_bl("2", dir),
-            [dir] if dir == "dec" => change_bl("2",dir),
+            [dir] if dir == "inc" => change_bl("2", inc),
+            [dir] if dir == "dec" => change_bl("2", dec),
             _ => print_help(),
         }
     } else {
@@ -73,10 +79,11 @@ fn main() {
     }
 }
 
-fn calculate_change(current: u16, max: u16, step_size: u16, dir: &str) -> u16 {
+fn calculate_change(current: u16, max: u16, step_size: u16, dir: Direction) -> u16 {
     let step: u16 = (max as f32 * (step_size as f32 / 100.0 )) as u16;
-    let change: u16 = {
-        if dir == "dec" {current.saturating_sub(step)} else {current.saturating_add(step)}
+    let change: u16 = match dir {
+            Direction::Inc => current.saturating_add(step),
+            Direction::Dec => current.saturating_sub(step),
     };
 
     if change > max {
@@ -87,7 +94,7 @@ fn calculate_change(current: u16, max: u16, step_size: u16, dir: &str) -> u16 {
 
 }
 
-fn change_bl(step_size: &str, dir: &str) {
+fn change_bl(step_size: &str, dir: Direction) {
     let step_size: u16 = match step_size.parse() {
         Ok(n) => n,
         Err(_) => {
