87e80cebd49e03a5826f99f1d3a391a8e09d04fe
VoltaireNoir <mzahmed95@gmail.com>
2022-07-18 22:24:42 +0530

    Use colors for stdout

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use blight::{Device, Direction};
 use std::env;
-use blight::{Direction,Device};
+use colored::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,10 +8,10 @@ fn main() {
 
     if args.len() > 1 {
         match &args[1..] {
-            [dir,step_size] if dir == "inc" => change_bl(step_size, inc),
-            [dir,step_size] if dir == "dec" => change_bl(step_size, dec),
-            [case,value] if case == "set" => set_bl(value),
-            [case,value] if case == "set" => set_bl(value),
+            [dir, step_size] if dir == "inc" => change_bl(step_size, inc),
+            [dir, step_size] if dir == "dec" => change_bl(step_size, dec),
+            [case, value] if case == "set" => set_bl(value),
+            [case, value] if case == "set" => set_bl(value),
             [dir] if dir == "inc" => change_bl("2", inc),
             [dir] if dir == "dec" => change_bl("2", dec),
             _ => print_help(),
@@ -21,10 +22,10 @@ fn main() {
 }
 
 fn calculate_change(current: u16, max: u16, step_size: u16, dir: Direction) -> u16 {
-    let step: u16 = (max as f32 * (step_size as f32 / 100.0 )) as u16;
+    let step: u16 = (max as f32 * (step_size as f32 / 100.0)) as u16;
     let change: u16 = match dir {
-            Direction::Inc => current.saturating_add(step),
-            Direction::Dec => current.saturating_sub(step),
+        Direction::Inc => current.saturating_add(step),
+        Direction::Dec => current.saturating_sub(step),
     };
 
     if change > max {
@@ -32,16 +33,15 @@ fn calculate_change(current: u16, max: u16, step_size: u16, dir: Direction) -> u
     } else {
         change
     }
-
 }
 
 fn change_bl(step_size: &str, dir: Direction) {
     let step_size: u16 = match step_size.parse() {
         Ok(n) => n,
         Err(_) => {
-            println!("Invalid step size: use a positive integer");
-            return
-        },
+            println!("{}","Invalid step size: use a positive integer".red().bold());
+            return;
+        }
     };
     let device = Device::new();
     let change = calculate_change(device.current, device.max, step_size, dir);
@@ -54,8 +54,8 @@ fn set_bl(val: &str) {
     let val: u16 = match val.parse() {
         Ok(n) => n,
         Err(_) => {
-            println!("Invalid value: use a positive integer");
-            return
+            println!("{}","Invalid value: use a positive integer".red().bold());
+            return;
         }
     };
     let device = Device::new();
@@ -65,14 +65,12 @@ fn set_bl(val: &str) {
 }
 
 fn print_help() {
-    let help_str = "blight automatically detects GPU device, and updates brightness accordingly.\n
-blight inc [ocerride step size] - increase brightness
+    let title = "blight automatically detects GPU device, and updates brightness accordingly.";
+    let commands = "blight inc [ocerride step size] - increase brightness
 blight dec [override step size] - decrease brightness
-blight set [value] - set custom brightness value
-
-Examples:
+blight set [value] - set custom brightness value";
+    let exampels = "Examples:
     blight inc (increases brightness by 2% - default step size)
-    blight dec 10 (increases brightness by 10%)
-    ";
-    println!("{help_str}")
+    blight dec 10 (increases brightness by 10%)";
+    println!("{}\n\n{}\n\n{}",title.blue().bold(),commands.magenta(),exampels.bright_yellow());
 }
