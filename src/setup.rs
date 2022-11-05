681d23e602d244b4d6fe95fb27dacdfc342238b5
VoltaireNoir <mzahmed95@gmail.com>
2022-11-05 11:32:33 +0530

    Format setup code

--- src/setup.rs
+++ src/setup.rs
@@ -6,10 +6,10 @@
 use colored::*;
 use std::{
     error::Error,
+    fs,
     io::{self, ErrorKind},
     path::PathBuf,
     process,
-    fs,
 };
 
 const RULES: &str = r#"ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"
@@ -19,46 +19,47 @@ const UDEVFILE: &str = "/lib/udev/rules.d/90-blight.rules";
 /// The function runs the setup. The udev file 90-blight.rules is placed in /lib/udev/.udev.rules.d/.
 /// The user is added to the 'video' group if they're not already in it.
 pub fn run() {
-    println!("{}","Running Setup".bold());
+    println!("{}", "Running Setup".bold());
     print!("UDEV Rules: ");
     match setup_rules() {
-        RulesResult::Ok => println!("{}","Ok".green()),
-        RulesResult::Exists => println!("{}","Ok (already in place)".green()),
+        RulesResult::Ok => println!("{}", "Ok".green()),
+        RulesResult::Exists => println!("{}", "Ok (already in place)".green()),
         RulesResult::Err(err) => {
             if err.kind() == ErrorKind::PermissionDenied {
-                println!("{}",
-                         "Failed. Run `blight setup` with sudo.".red())
+                println!("{}", "Failed. Run `blight setup` with sudo.".red())
             } else {
-                println!("{} {}","Error:".red(),err);
+                println!("{} {}", "Error:".red(), err);
             }
         }
     }
     print!("Video Group: ");
     match setup_group() {
-        GroupResult::Exists => println!("{}","Ok (already in group)".green()),
-        GroupResult::Err(err) => println!("{} {}","Error:".red(),err),
+        GroupResult::Exists => println!("{}", "Ok (already in group)".green()),
+        GroupResult::Err(err) => println!("{} {}", "Error:".red(), err),
         GroupResult::UnknownErr => println!("{}", "Failed. Run `blight setup` with sudo.".red(),),
-        GroupResult::Ok => println!("{}","Ok".green())
+        GroupResult::Ok => println!("{}", "Ok".green()),
     }
 
-    println!("{}\n{}",
-             "Recommended: Reboot your system once the setup completes successfully.".yellow(),
-             "You can run `blight status` to check if you have gained write permissions.".yellow());
+    println!(
+        "{}\n{}",
+        "Recommended: Reboot your system once the setup completes successfully.".yellow(),
+        "You can run `blight status` to check if you have gained write permissions.".yellow()
+    );
 }
 
 enum RulesResult {
     Ok,
     Exists,
-    Err(std::io::Error)
+    Err(std::io::Error),
 }
 
 fn setup_rules() -> RulesResult {
     let path = PathBuf::from(UDEVFILE);
     if path.exists() && fs::read_to_string(&path).unwrap().contains(RULES) {
-        return RulesResult::Exists
+        return RulesResult::Exists;
     }
     if let Err(err) = fs::write(UDEVFILE, RULES) {
-        return RulesResult::Err(err)
+        return RulesResult::Err(err);
     }
     RulesResult::Ok
 }
@@ -71,38 +72,35 @@ enum GroupResult {
 }
 
 fn setup_group() -> GroupResult {
-    let user = String::from_utf8(
-        process::Command::new("logname")
-            .output()
-            .unwrap()
-            .stdout)
-        .unwrap();
+    let user =
+        String::from_utf8(process::Command::new("logname").output().unwrap().stdout).unwrap();
 
     if in_group(&user) {
-        return GroupResult::Exists
+        return GroupResult::Exists;
     }
 
-    if fs::read_to_string("/etc/group")
-        .unwrap()
-        .contains("video") {
-            if let Err(err) = add_to_group(&user) {
-                    return GroupResult::Err(Box::new(err))
-                }
-        } else {
-            if let Err(err) = process::Command::new("groupadd")
-                .arg("video")
-                .stderr(process::Stdio::null())
-                .output() {
-                    return GroupResult::Err(Box::new(err))
-                }
-            if let Err(err) = add_to_group(&user) {
-                    return GroupResult::Err(Box::new(err))
-                }
+    if fs::read_to_string("/etc/group").unwrap().contains("video") {
+        if let Err(err) = add_to_group(&user) {
+            return GroupResult::Err(Box::new(err));
         }
+    } else {
+        if let Err(err) = process::Command::new("groupadd")
+            .arg("video")
+            .stderr(process::Stdio::null())
+            .output()
+        {
+            return GroupResult::Err(Box::new(err));
+        }
+        if let Err(err) = add_to_group(&user) {
+            return GroupResult::Err(Box::new(err));
+        }
+    }
 
-    in_group(&user)
-        .then_some(GroupResult::Ok)
-        .unwrap_or(GroupResult::UnknownErr)
+    if in_group(&user) {
+        GroupResult::Ok
+    } else {
+        GroupResult::UnknownErr
+    }
 }
 
 fn in_group(user: &str) -> bool {
@@ -111,14 +109,15 @@ fn in_group(user: &str) -> bool {
             .arg(user.trim())
             .output()
             .expect("Failed to run groups command")
-            .stdout)
-        .unwrap()
-        .contains("video")
+            .stdout,
+    )
+    .unwrap()
+    .contains("video")
 }
 
 fn add_to_group(user: &str) -> Result<(), io::Error> {
     process::Command::new("usermod")
-        .args(["-aG","video",user.trim()])
+        .args(["-aG", "video", user.trim()])
         .stderr(process::Stdio::null())
         .output()?;
     Ok(())
