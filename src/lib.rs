8952d418bbb66863b24aeba9724c5582bd77f410
VoltaireNoir <mzahmed95@gmail.com>
2022-11-05 11:33:05 +0530

    Use new parser, separate error handling

--- src/lib.rs
+++ src/lib.rs
@@ -14,21 +14,20 @@
 //! Run `blight` in terminal to display all supported commands and options
 
 use colored::*;
-use std::{
-    env, fs,
-    path::PathBuf,
-    process::{self, Command},
-    thread,
-    time::Duration,
-};
+use err::BlibError;
+use std::{env, fs, path::PathBuf, process::Command, thread, time::Duration};
 
+pub mod err;
 pub mod setup;
 
 const BLDIR: &str = "/sys/class/backlight";
 const SAVEDIR: &str = "/.local/share/blight";
 
+type BlResult<T> = Result<T, BlibError>;
+
 /// This enum is used to specify the direction in which the backlight should be changed.
 /// Inc -> Increase, Dec -> Decrease.
+#[derive(Clone, Copy)]
 pub enum Direction {
     Inc,
     Dec,
@@ -36,9 +35,11 @@ pub enum Direction {
 
 /// This enum is used to specify the kind of backlight change to carry out. \
 /// Regular change applies the calculated change directly, whereas the sweep change occurs in incremental steps.
+#[derive(Default)]
 pub enum Change {
-    Sweep,
+    #[default]
     Regular,
+    Sweep,
 }
 
 /// Contains name of the detected GPU device and its current and max brightness values.
@@ -54,37 +55,39 @@ impl Device {
     /// Creates a new Device instance by reading values from /sys/class/backlight/ directory based on the detected GPU device.\
     /// Returns the Device struct wrapped in Some() or returns None when no known device is detected \
     /// This is how the devices are priorirized AmdGPU or Intel > Nvdia > ACPI > Any Fallback Device
-    pub fn new(name: Option<String>) -> Option<Device> {
+    pub fn new(name: Option<String>) -> BlResult<Device> {
         let name = if let Some(n) = name {
             PathBuf::from(format!("{BLDIR}/{n}/brightness"))
                 .is_file()
-                .then_some(n)?
+                .then_some(n)
+                .ok_or(BlibError::NoDeviceFound)?
         } else {
             Self::detect_device(BLDIR)?
         };
-        Some(Self::load(name))
+        let device = Self::load(name)?;
+        Ok(device)
     }
 
-    fn load(name: String) -> Device {
+    fn load(name: String) -> BlResult<Device> {
         let device_dir = format!("{BLDIR}/{name}");
-        Device {
-            current: Self::get_current(&device_dir),
-            max: Self::get_max(&device_dir),
+        Ok(Device {
+            current: Self::get_current(&device_dir)?,
+            max: Self::get_max(&device_dir)?,
             device_dir,
             name,
-        }
+        })
     }
 
-    fn detect_device(bldir: &str) -> Option<String> {
+    fn detect_device(bldir: &str) -> BlResult<String> {
         let dirs = fs::read_dir(bldir).expect("Failed to read dir");
-        let (mut nv, mut ac, mut fl): (Option<String>, Option<String>, Option<String>) = (None, None, None);
+        let (mut nv, mut ac, mut fl): (Option<String>, Option<String>, Option<String>) =
+            (None, None, None);
 
         for entry in dirs {
             let name = entry.unwrap().file_name();
             if let Some(name) = name.to_str() {
-
                 if name.contains("amdgpu") || name.contains("intel") {
-                    return Some(name.to_string());
+                    return Ok(name.to_string());
                 } else if nv.is_none() && (name.contains("nvidia") | name.contains("nv")) {
                     nv = Some(name.to_string());
                 } else if ac.is_none() && name.contains("acpi") {
@@ -95,56 +98,49 @@ impl Device {
             }
         }
 
-        if nv.is_some() {
-            nv
-        } else if ac.is_some() {
-            ac
+        if let Some(nv) = nv {
+            Ok(nv)
+        } else if let Some(ac) = ac {
+            Ok(ac)
+        } else if let Some(fl) = fl {
+            Ok(fl)
         } else {
-            fl
+            Err(BlibError::NoDeviceFound)
         }
     }
 
-    fn get_max(device_dir: &str) -> u16 {
+    fn get_max(device_dir: &str) -> BlResult<u16> {
         let max: u16 = fs::read_to_string(format!("{device_dir}/max_brightness"))
-            .expect("Failed to read max value")
+            .or(Err(BlibError::ReadMax))?
             .trim()
             .parse()
-            .expect("Failed to parse max value");
-        max
+            .or(Err(BlibError::ReadMax))?;
+        Ok(max)
     }
 
-    fn get_current(device_dir: &str) -> u16 {
+    fn get_current(device_dir: &str) -> BlResult<u16> {
         let current: u16 = fs::read_to_string(format!("{device_dir}/brightness"))
-            .expect("Failed to read max value")
+            .or(Err(BlibError::ReadCurrent))?
             .trim()
             .parse()
-            .expect("Failed to parse max value");
-        current
+            .or(Err(BlibError::ReadCurrent))?;
+        Ok(current)
     }
     /// This method is used to write to the brightness file containted in /sys/class/backlight/ dir of the respective detected device.\
     /// It takes in a brightness value, and writes to othe relavant brightness file.
-    pub fn write_value(&self, value: u16) {
-        if let Err(err) = fs::write(
+    pub fn write_value(&self, value: u16) -> BlResult<()> {
+        fs::write(
             format!("{}/brightness", self.device_dir),
             format!("{value}"),
-        ) {
-            let tip = format!(
-                "\
-Make sure you have write permissions for the file
-'{BLDIR}/{}/brightness'
-Run `sudo blight setup` to install necessarry udev rules and add user to video group.
-Or visit https://wiki.archlinux.org/title/Backlight#Hardware_interfaces
-if you'd like to do it manually.",
-                self.name
-            )
-            .yellow();
-            eprintln!(
-                "{} {}\nTip: {}",
-                "Error:".red().bold(),
-                err.to_string().red().bold(),
-                tip,
-            )
-        }
+        )
+        .or_else(|err| {
+            Err(BlibError::WriteNewVal {
+                err,
+                dev: self.name.clone(),
+            })
+        })?;
+
+        Ok(())
     }
 }
 
@@ -164,82 +160,49 @@ pub fn calculate_change(current: u16, max: u16, step_size: u16, dir: &Direction)
     }
 }
 
-trait ErrorHandler {
-    type ReturnTarget: ?Sized;
-    fn err_handler(self) -> Self::ReturnTarget;
-}
-
-impl ErrorHandler for Option<Device> {
-    type ReturnTarget = Device;
-    fn err_handler(self) -> Self::ReturnTarget {
-        self.unwrap_or_else(|| {
-            eprintln!(
-                "{}",
-                "Error: No known device detected on system".red().bold()
-            );
-            process::exit(1)
-        })
-    }
-}
-
-impl ErrorHandler for Result<u16, std::num::ParseIntError> {
-    type ReturnTarget = u16;
-    fn err_handler(self) -> Self::ReturnTarget {
-        self.unwrap_or_else(|_| {
-            eprintln!(
-                "{}",
-                "Invalid step size: use a positive integer".red().bold()
-            );
-            process::exit(1)
-        })
-    }
-}
-
 /// Changes backlight based on step-size (percentage), change type and direction.
 /// Regular change uses calculated change value based on step size and is applied instantly
 /// Sweep change on the other hand, occurs gradually, producing a fade or sweeping effect.
-pub fn change_bl(step_size: &str, ch: Change, dir: Direction, device_name: Option<String>) {
-    let step_size: u16 = step_size.parse().err_handler();
-
-    let device = Device::new(device_name).err_handler();
+pub fn change_bl(
+    step_size: u16,
+    ch: Change,
+    dir: Direction,
+    device_name: Option<String>,
+) -> Result<(), BlibError> {
+    let device = Device::new(device_name)?;
 
     let change = calculate_change(device.current, device.max, step_size, &dir);
     if change != device.current {
         match ch {
-            Change::Sweep => sweep(&device, change, &dir),
-            Change::Regular => device.write_value(change),
+            Change::Sweep => sweep(&device, change, &dir)?,
+            Change::Regular => device.write_value(change)?,
         }
     }
+    Ok(())
 }
 
 /// This function takes a brightness value, creates a Device struct, and writes the value to the brightness file
 /// as long as the given value falls under the min and max bounds.
 /// Unlike change_bl, this function does not calculate any change, it writes the given value directly.
-pub fn set_bl(val: Option<String>, device_name: Option<String>) {
-    let val: u16 = if let Some(val) = val {
-        val.parse().err_handler()
-    } else {
-        eprintln!("{}","Error: Missing argument. No value provided to set.".red().bold());
-        return ;
-    };
+pub fn set_bl(val: u16, device_name: Option<String>) -> Result<(), BlibError> {
+    let device = Device::new(device_name)?;
 
-    let device = Device::new(device_name).err_handler();
-
-    if (val <= device.max) & (val != device.current) {
-        device.write_value(val);
+    if val <= device.max && val != device.current {
+        device.write_value(val)?;
     }
+    Ok(())
 }
 
 /// This function takes a borrow of Device struct, a calculated change value and the direction.
 /// It writes to the relavant brightness file in an increment of 1 on each loop until change value is reached.
 /// Each loop has a delay of 25ms, to produce to a smooth sweeping effect when executed.
-pub fn sweep(device: &Device, change: u16, dir: &Direction) {
+pub fn sweep(device: &Device, change: u16, dir: &Direction) -> Result<(), BlibError> {
     match dir {
         Direction::Inc => {
             let mut val = device.current + 1;
 
             while val <= change {
-                device.write_value(val);
+                device.write_value(val)?;
                 thread::sleep(Duration::from_millis(25));
                 val += 1;
             }
@@ -248,7 +211,7 @@ pub fn sweep(device: &Device, change: u16, dir: &Direction) {
             let mut val = device.current - 1;
 
             while val >= change {
-                device.write_value(val);
+                device.write_value(val)?;
                 thread::sleep(Duration::from_millis(25));
                 if val == 0 {
                     break;
@@ -257,81 +220,43 @@ pub fn sweep(device: &Device, change: u16, dir: &Direction) {
             }
         }
     }
+    Ok(())
 }
 
 /// Saves current brightness value to "$HOME/.local/share/blight/save"
-pub fn save(device_name: Option<String>) {
-    let device = Device::new(device_name).err_handler();
+pub fn save(device_name: Option<String>) -> Result<(), BlibError> {
+    let device = Device::new(device_name)?;
     let mut savedir = PathBuf::from(env::var("HOME").unwrap() + SAVEDIR);
 
-    if !savedir.exists() {
-        if fs::create_dir_all(&savedir).is_err() {
-            eprintln!(
-                "{} {}",
-                "Error: Failed to create save directory at:".red().bold(),
-                savedir.display()
-            );
-            return;
-        }
+    if !savedir.exists() && fs::create_dir_all(&savedir).is_err() {
+        return Err(BlibError::CreateSaveDir(savedir));
     }
 
     savedir.push("blight.save");
 
     if fs::write(&savedir, format!("{} {}", device.name, device.current)).is_err() {
-        eprintln!(
-            "{}\n{}",
-            "Error: Failed to write to save file at".red().bold(),
-            savedir.display()
-        );
-        return;
+        return Err(BlibError::WriteToSaveFile(savedir));
     };
 
-    println!("{}", "Current brightness successfully saved".green())
+    Ok(())
 }
 
 /// Restores brightness value from "$HOME/.local/share/blight/save" if it exists.
-pub fn restore() {
+pub fn restore() -> Result<(), BlibError> {
     let save = PathBuf::from((env::var("HOME").unwrap() + SAVEDIR) + "/blight.save");
-    let restore;
 
-    if save.is_file() {
-        match fs::read_to_string(save) {
-            Ok(s) => restore = s,
-            Err(err) => {
-                eprintln!(
-                    "{}\n{}",
-                    "Error: Failed to read from save file".red().bold(),
-                    err
-                );
-                return;
-            }
-        }
+    let restore = if save.is_file() {
+        fs::read_to_string(save).map_err(BlibError::ReadFromSave)?
     } else {
-        eprintln!(
-            "{}\n{}",
-            "Error: No saved state found".red().bold(),
-            "Tip: Try using `blight save` first".yellow()
-        );
-        return;
-    }
-
-    let (device_name, val) = restore.split_once(" ").unwrap();
-    let device = Device::new(Some(device_name.to_string())).err_handler();
-
-    let value: u16 = if let Ok(v) = val.parse() {
-        v
-    } else {
-        eprintln!(
-            "{}\n{}",
-            "Error: Failed to parse saved brightness value."
-                .red()
-                .bold(),
-            "Tip: The saved state data might be corrupt. Try using `blight save` again.".yellow()
-        );
-        return;
+        return Err(BlibError::NoSaveFound);
     };
-    device.write_value(value);
-    println!("{}", "Brightness successfully restored".green());
+
+    let (device_name, val) = restore.split_once(' ').unwrap();
+    let device = Device::new(Some(device_name.to_string()))?;
+
+    let value: u16 = val.parse().or(Err(BlibError::SaveParseErr))?;
+    device.write_value(value)?;
+    Ok(())
 }
 
 /// This function is the current way of determining whether another instance of blight is running.
@@ -339,20 +264,11 @@ pub fn restore() {
 pub fn is_running() -> bool {
     let out = Command::new("pgrep")
         .arg("-x")
-        .arg(
-            env::current_exe()
-                .unwrap()
-                .file_name()
-                .unwrap()
-        )
+        .arg(env::current_exe().unwrap().file_name().unwrap())
         .output()
         .expect("Process command failed");
     let out = String::from_utf8(out.stdout).expect("Failed to convert");
-    if out.trim().len() > 6 {
-        true
-    } else {
-        false
-    }
+    out.trim().len() > 6
 }
 
 fn check_write_perm(device_name: &str, bldir: &str) -> Result<(), std::io::Error> {
@@ -362,10 +278,9 @@ fn check_write_perm(device_name: &str, bldir: &str) -> Result<(), std::io::Error
         .and(Ok(()))
 }
 
-
 /// This function creates a Device instance and prints the detected device, along with its current and max brightness values.
-pub fn print_status(device_name: Option<String>) {
-    let device = Device::new(device_name).err_handler();
+pub fn print_status(device_name: Option<String>) -> Result<(), BlibError> {
+    let device = Device::new(device_name)?;
 
     let write_perm = match check_write_perm(&device.name, BLDIR) {
         Ok(_) => "Ok".green(),
@@ -380,6 +295,7 @@ pub fn print_status(device_name: Option<String>) {
         device.current.to_string().green(),
         device.max.to_string().green()
     );
+    Ok(())
 }
 
 /// Reads backlight directory (sys/class/backlight) and prints it's contents
@@ -430,7 +346,10 @@ Examples:
 }
 
 pub fn print_welcome() {
-    println!("{}\n","blight: A backlight utility for Linux".blue().bold());
+    println!(
+        "{}\n",
+        "blight: A backlight utility for Linux".blue().bold()
+    );
     let cc = "\
 inc -> increase brightness by 2%
 dec -> decrease brightness by 2%
@@ -440,8 +359,11 @@ sweep-down -> decrease brightness smoothly by 10%
 status -> show backlight device info
 setup -> gain write permission to brightness file
 ";
-    println!("{}\n{}","Common Commands".bold(), cc.green().bold());
-    println!("{}","Use `blight help' to display all commands and options".yellow())
+    println!("{}\n{}", "Common Commands".bold(), cc.green().bold());
+    println!(
+        "{}",
+        "Use `blight help' to display all commands and options".yellow()
+    )
 }
 
 #[cfg(test)]
@@ -455,7 +377,7 @@ mod tests {
         clean_up();
         setup_test_env(&["nvidia_0", "generic"]).unwrap();
         let name = Device::detect_device(TESTDIR);
-        assert!(name.is_some());
+        assert!(name.is_ok());
         assert_eq!(name.unwrap(), "nvidia_0");
         clean_up();
     }
@@ -465,7 +387,7 @@ mod tests {
         clean_up();
         setup_test_env(&["nvidia_0", "generic", "amdgpu_x"]).unwrap();
         let name = Device::detect_device(TESTDIR);
-        assert!(name.is_some());
+        assert!(name.is_ok());
         assert_eq!(name.unwrap(), "amdgpu_x");
         clean_up();
     }
@@ -475,7 +397,7 @@ mod tests {
         clean_up();
         setup_test_env(&["acpi_video0", "generic"]).unwrap();
         let name = Device::detect_device(TESTDIR);
-        assert!(name.is_some());
+        assert!(name.is_ok());
         assert_eq!(name.unwrap(), "acpi_video0");
         clean_up();
     }
@@ -485,7 +407,7 @@ mod tests {
         clean_up();
         setup_test_env(&["generic"]).unwrap();
         let name = Device::detect_device(TESTDIR);
-        assert!(name.is_some());
+        assert!(name.is_ok());
         assert_eq!(name.unwrap(), "generic");
         clean_up();
     }
@@ -500,7 +422,7 @@ mod tests {
             current: 50,
             device_dir: format!("{TESTDIR}/generic"),
         };
-        d.write_value(100);
+        d.write_value(100).unwrap();
         let r = fs::read_to_string(format!("{TESTDIR}/generic/brightness"))
             .expect("failed to read test backlight value");
         let res = r.trim();
@@ -512,7 +434,7 @@ mod tests {
     fn current_value() {
         clean_up();
         setup_test_env(&["generic"]).unwrap();
-        let current = Device::get_current(&format!("{TESTDIR}/generic"));
+        let current = Device::get_current(&format!("{TESTDIR}/generic")).unwrap();
         assert_eq!(current.to_string(), "50");
         clean_up();
     }
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,23 @@
-use blight::{
-    Change::{Regular,Sweep},
-    Direction::{Dec,Inc},
-    setup,
-};
-use colored::Colorize;
-use std::env::{self,Args};
+use std::env;
+
+mod utils;
 
 fn main() {
     if blight::is_running() {
-        eprintln!("{} Another instance of blight is already running.",
-                  "Error:".red().bold());
+        utils::print_err("Another instance of blight is already running");
         return;
     }
-    argument_parser(env::args());
-}
 
-fn argument_parser(mut args: Args) {
-    if let Some(arg) = args.next().and_then(|_| args.next()) {
-        match &arg[..] {
-            "help" => blight::print_help(),
-            "status" => blight::print_status(args.next()),
-            "list" => blight::print_devices(),
-            "save" => blight::save(args.next()),
-            "restore" => blight::restore(),
-            "setup" => setup::run(),
-            "set" => blight::set_bl(args.next(), args.next()),
-            "inc" => {
-                if let Some(v) = args.next() {
-                    blight::change_bl(&v, Regular, Inc, args.next())
-                } else {
-                    blight::change_bl("2", Regular, Inc, args.next())
-                }
-            },
-            "dec" => {
-                if let Some(v) = args.next() {
-                    blight::change_bl(&v, Regular, Dec, args.next())
-                } else {
-                    blight::change_bl("2", Regular, Dec, args.next())
-                }
-            },
-            "sweep-up" => {
-                if let Some(v) = args.next() {
-                    blight::change_bl(&v, Sweep, Inc, args.next())
-                } else {
-                    blight::change_bl("10", Sweep, Inc, args.next())
-                }
-            },
-            "sweep-down" => {
-                if let Some(v) = args.next() {
-                    blight::change_bl(&v, Sweep, Dec, args.next())
-                } else {
-                    blight::change_bl("10", Sweep, Dec, args.next())
-                }
-            },
-            _ => println!("{}\n{}",
-                          "Oops... You have entered an unrecognised command".bold(),
-                          "Tip: Try `blight help` to see all supported commands".yellow(),
-            )
+    let config = match utils::parse(env::args().skip(1)) {
+        Ok(c) => c,
+        Err(e) => {
+            utils::print_err(e);
+            return;
         }
-    } else {
-        blight::print_welcome();
+    };
+
+    match utils::execute(config) {
+        Err(e) => utils::print_err(e),
+        Ok(msg) => utils::print_ok(msg),
     }
 }
